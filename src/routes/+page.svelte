5c158ec15a9cae3552a282f4000871206708adf2
niiwade <niilantewade77@gmail.com>
2022-09-13 03:30:06 +0000

    added new changes

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to SvelteKit</h1>
+<p class="bg-gradient-to-r from-indigo-500">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['src/**/*.{html,css,js,jsx,ts,tsx,svelte}'],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+};
